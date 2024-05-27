9ed9d4139a6355b39b5f026dfb0862b19de79ab7
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2024-05-27 14:21:02 -0400

    Generate gem scaffolding

---
+++ lib/mermaid_erd_markdown.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mermaid_erd_markdown/version"
+
+module Rails
+  module MermaidErdMarkdown
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
